73e2080950f0afb4d9767522c652fd3b789947fc
BallaMoussa9 <ballamoussasinayoko@gmail.com>
2026-01-28 10:00:38 -0800

    Ajout d'une modification a l'envoie de mail

--- app/Mail/MailForUser.php
+++ app/Mail/MailForUser.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class MailForUser extends Mailable implements ShouldQueue 
+class MailForUser extends Mailable 
 {
     use Queueable, SerializesModels;
 
